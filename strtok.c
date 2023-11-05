fdbea9a167333ecae86f03232c3fc074dafc772d
mayaassem5 <mayaassem5@gmail.com>
2023-11-05 18:48:41 +0200

    edit 3

--- strtok.c
+++ strtok.c
@@ -36,10 +36,8 @@ unsigned int _strspn(char *s, char *accept)
 
 char *_strtok(char *str, char *delimiters)
 {
-	static char *nextToken;
 	char *token;
-
-	*nextToken = '\0';
+    static char *nextToken = NULL;
 
 	if (str != NULL)
 	{
